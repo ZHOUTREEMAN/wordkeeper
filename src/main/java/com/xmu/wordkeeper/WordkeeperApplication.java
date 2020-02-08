c2cb88970db5a52198c6d383e951d174ed7c3419
ZHOUTREEMAN <329201962@qq.com>
2020-02-08 21:03:52 +0800

    Initial commit

---
+++ src/main/java/com/xmu/wordkeeper/WordkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.xmu.wordkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordkeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordkeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmu/wordkeeper/WordkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmu.wordkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordkeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
