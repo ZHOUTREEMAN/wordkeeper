f7f275656926a3038fca1feda0b365767f6805e3
linlianhui <linlianhui@email.com>
2020-02-09 16:03:16 +0800

    "'llh-add dao file'" by linlianhui

---
+++ src/main/java/com/xmu/wordkeeper/dao/PunchDao.java
@@ -0,0 +1,92 @@
+package com.xmu.wordkeeper.dao;
+
+import com.xmu.wordkeeper.domain.Punch;
+import com.xmu.wordkeeper.mapper.PunchMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author linlianhui
+ * @date 2020/02/09
+ */
+@Repository
+public class PunchDao {
+
+    @Autowired
+    private PunchMapper punchMapper;
+
+    /**
+     * 添加 punch
+     *
+     * @param punch 待添加 punch
+     * @return 操作状态码
+     */
+    int insertPunch(Punch punch)
+    {
+        punch.setGmtCreate(LocalDateTime.now());
+        punch.setGmtModified(LocalDateTime.now());
+        punch.setIsDeleted(false);
+
+        return punchMapper.insertPunch(punch);
+    }
+
+    /**
+     * 删除 punch
+     *
+     * @param id 主键
+     * @return 操作状态码
+     */
+    int deletePunchById(Integer id)
+    {
+        Punch punch = punchMapper.selectPunchById(id);
+        if(null != punch)
+        {
+            punch.setGmtModified(LocalDateTime.now());
+            punchMapper.updatePunch(punch);
+        }
+
+        return punchMapper.deletePunchById(id);
+    }
+
+    /**
+     * 更新 punch
+     *
+     * @param punch 待更新 punch
+     * @return 操作状态码
+     */
+    int updatePunch(Punch punch)
+    {
+        Punch temp = punchMapper.selectPunchById(punch.getId());
+        if(null != temp)
+        {
+            punch.setGmtCreate(temp.getGmtCreate());
+            punch.setGmtModified(LocalDateTime.now());
+        }
+
+        return punchMapper.updatePunch(punch);
+    }
+
+    /**
+     * 通过主键查询 punch
+     *
+     * @param id 主键
+     * @return Punch
+     */
+    Punch selectPunchById(Integer id)
+    {
+        return punchMapper.selectPunchById(id);
+    }
+
+    /**
+     * 查询所有 punch
+     *
+     * @return Punch列表
+     */
+    List<Punch> selectAllPunches()
+    {
+        return punchMapper.selectAllPunches();
+    }
+}
---
+++ src/main/java/com/xmu/wordkeeper/dao/UserDao.java
@@ -0,0 +1,92 @@
+package com.xmu.wordkeeper.dao;
+
+import com.xmu.wordkeeper.domain.User;
+import com.xmu.wordkeeper.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author linlianhui
+ * @date 2020/02/09
+ */
+@Repository
+public class UserDao {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 添加 User
+     *
+     * @param user 待添加用户
+     * @return 操作状态码
+     */
+    int insertUser(User user)
+    {
+        user.setGmtCreate(LocalDateTime.now());
+        user.setGmtModified(LocalDateTime.now());
+        user.setIsDeleted(false);
+
+        return userMapper.insertUser(user);
+    }
+
+    /**
+     * 删除 User
+     *
+     * @param id 主键
+     * @return 操作状态码
+     */
+    int deleteUserById(Integer id)
+    {
+        User user = userMapper.selectUserById(id);
+        if(null != user)
+        {
+            user.setGmtModified(LocalDateTime.now());
+            userMapper.updateUser(user);
+        }
+
+        return userMapper.deleteUserById(id);
+    }
+
+    /**
+     * 更新 User
+     *
+     * @param user 待更新用户
+     * @return 操作状态码
+     */
+    int updateUser(User user)
+    {
+        User temp = userMapper.selectUserById(user.getId());
+        if(null != temp)
+        {
+            user.setGmtCreate(temp.getGmtCreate());
+            user.setGmtModified(LocalDateTime.now());
+        }
+
+        return userMapper.updateUser(user);
+    }
+
+    /**
+     * 通过主键查询 User
+     *
+     * @param id 主键
+     * @return User
+     */
+    User selectUserById(Integer id)
+    {
+        return userMapper.selectUserById(id);
+    }
+
+    /**
+     * 查询所有 User
+     *
+     * @return User列表
+     */
+    List<User> selectAllUsers()
+    {
+        return userMapper.selectAllUsers();
+    }
+}
