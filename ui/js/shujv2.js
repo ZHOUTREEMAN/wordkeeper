4644fc15acab95c437ef6acd40c5618a7f104199
CFH <1504403484@qq.com>
2020-02-16 21:16:44 +0800

    -

---
+++ ui/js/shujv2.js
@@ -0,0 +1,21 @@
+var data = [ {
+	"word":"intellect",
+	"interpretation":"n.(尤指高等的)智力，思维逻辑领悟力;智力高的人;才智超群的人",
+	"mastery":"66%",
+	"createDate":"2020-01-26 19:12:48"
+  }, {
+    "word":"transmission",
+	"interpretation":"n.传送;传递;传达;传播;传染;(电子信号、信息或广播、电视节目的)播送，发射，发送，传输;(电台或电视)信息，广播",
+	"mastery":"45%",
+	"createDate":"2020-01-26 19:12:48"
+  }, {
+    "word":"motion",
+	"interpretation":"n.运动;移动;动;(为传递信息用手或头做的)动作;动议;提议 v.(以头或手)做动作，示意",
+	"mastery":"53%",
+	"createDate":"2020-01-26 19:12:48"
+  }, {
+    "word":"apply",
+	"interpretation":"v.(通常以书面形式)申请，请求;使用;应用;涂;敷;施",
+	"mastery":"83%",
+	"createDate":"2020-01-26 19:12:48"
+  }]
