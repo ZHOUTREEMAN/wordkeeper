9c706a5bb98b86ca05af162d79243a4f958899e6
CFH <1504403484@qq.com>
2020-02-16 21:30:00 +0800

    -

--- ui/js/shujv2.js
+++ ui/js/shujv2.js
@@ -3,19 +3,34 @@ var data = [ {
 	"interpretation":"n.(尤指高等的)智力，思维逻辑领悟力;智力高的人;才智超群的人",
 	"mastery":"66%",
 	"createDate":"2020-01-26 19:12:48"
-  }, {
-    "word":"transmission",
+	}, {
+	"word":"transmission",
 	"interpretation":"n.传送;传递;传达;传播;传染;(电子信号、信息或广播、电视节目的)播送，发射，发送，传输;(电台或电视)信息，广播",
 	"mastery":"45%",
 	"createDate":"2020-01-26 19:12:48"
-  }, {
-    "word":"motion",
-	"interpretation":"n.运动;移动;动;(为传递信息用手或头做的)动作;动议;提议 v.(以头或手)做动作，示意",
+	}, {
+	"word":"motion",
+	"interpretation":"n.运动;移动;动;(为传递信息用手或头做的)动作;动议;提议\nv.(以头或手)做动作，示意",
 	"mastery":"53%",
 	"createDate":"2020-01-26 19:12:48"
-  }, {
-    "word":"apply",
+	}, {
+	"word":"apply",
 	"interpretation":"v.(通常以书面形式)申请，请求;使用;应用;涂;敷;施",
 	"mastery":"83%",
 	"createDate":"2020-01-26 19:12:48"
+	}, {
+	"word":"innocent",
+	"interpretation":"adj.无辜的;清白的;无罪的;无辜受害的;成为牺牲品的;无恶意的;无冒犯之意的\nn.无辜者，单纯的人(尤指天真无邪的孩子)",
+	"mastery":"34%",
+	"createDate":"2020-01-28 14:33:09"
+	}, {
+	"word":"tempt",
+	"interpretation":"v.引诱;诱惑;劝诱;鼓动;怂恿;利诱",
+	"mastery":"78%",
+	"createDate":"2020-01-28 14:33:09"
+	}, {
+	"word":"sufficient",
+	"interpretation":"adj.足够的;充足的",
+	"mastery":"56%",
+	"createDate":"2020-01-28 14:33:09"
   }]
